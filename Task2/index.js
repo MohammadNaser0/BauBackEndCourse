1edb863f08654960c7e74ce68ef160d4e6e11d5e
MohammadNaser0 <32201002005@std.bau.edu.jo>
2025-02-23 14:29:32 +0300

    Task 2 Completed

---
+++ Task2/index.js
@@ -0,0 +1,28 @@
+let students = [] , failedStudents = [] ,successStudents = [] ;
+
+
+    function createStudent(name ,age ,mark) {
+        return {name : name ,age : age ,mark : mark};
+    }
+  
+            let omar = createStudent("Omar" ,21 ,40);
+            let sami = createStudent("Sami" ,18 ,90);
+            let yara = createStudent("Yara" ,20 ,75);
+            let ahmad = createStudent("Ahmad" ,20 ,0);
+            let salem = createStudent("salem" ,19,72);
+
+    students.push(omar ,sami ,yara ,ahmad ,salem);
+    console.log(students);
+   
+
+                students.forEach(Element => {
+                if(Element.mark < 50){
+                    failedStudents.push(Element)
+                }
+                else
+                    successStudents.push(Element)
+            })
+
+    console.log(failedStudents);
+    console.log(successStudents);
+    
